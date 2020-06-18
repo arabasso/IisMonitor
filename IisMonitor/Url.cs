8c69bb084f3690fba59225a17f542ca3c4f1332b
Raphael Basso <arabasso@yahoo.com.br>
2020-06-18 15:04:01 -0300

    Removed unused using directives.

--- IisMonitor/Url.cs
+++ IisMonitor/Url.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.IO;
 using System.Net;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace IisMonitor
